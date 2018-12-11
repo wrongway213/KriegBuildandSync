216b095937b058b7906472c20a96bdba98cb4523
Zackptg5 <zackptg5@comcast.net>
2018-12-11 17:32:48 -0500

    Redo stuff

--- krieg.sh
+++ krieg.sh
@@ -1,20 +1,11 @@
 #!/usr/bin/env bash
 
-export KRIEG_SCRIPT="true"
-
-KRIEG_ROOT=`pwd`
-
-TEXTRESET=$(tput sgr0)
-TEXTGREEN=$(tput setaf 2)
-TEXTRED=$(tput setaf 1)
-
 EchoRed () {
 	echo "${TEXTRED}$1${TEXTRESET}"
 }
 EchoGreen () {
 	echo "${TEXTGREEN}$1${TEXTRESET}"
 }
-
 Sync () {
   git fetch && git pull
   for i in "scripts" "OP5-OP5T" "AnyKernelBase" "Toolchains/aarch64-linux-android-4.9" "Toolchains/linux-x86"; do
@@ -23,17 +14,16 @@ Sync () {
 		  EchoGreen "$i directory found. Syncing."
 			echo ""
       cd "$i"
-      git reset --hard HEAD~1
-      git fetch && git pull
+      [[ $(git status) == *"Your branch is up to date"* ]] && git fetch && git pull
       cd ${KRIEG_ROOT}
     else
       echo ""
       EchoRed "$i directory not found. Cloning."
       echo ""
       case "$i" in
-        "scripts") git clone --depth=1 https://github.com/Krieg-Kernel/scripts.git "$i";;
-        "OP5-OP5T") git clone --depth=1 https://github.com/Krieg-Kernel/OP5-OP5T.git "$i";;
-        "AnyKernelBase") git clone --depth=1 https://github.com/Krieg-Kernel/AnyKernelBase.git "$i";;
+        "scripts") git clone$depth https://github.com/Krieg-Kernel/scripts.git "$i";;
+        "OP5-OP5T") git clone$depth https://github.com/Krieg-Kernel/OP5-OP5T.git "$i";;
+        "AnyKernelBase") git clone$depth https://github.com/Krieg-Kernel/AnyKernelBase.git "$i";;
         "Toolchains/aarch64-linux-android-4.9") git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 "$i";;
         "Toolchains/linux-x86") git clone --depth=1 https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 "$i";;
       esac
@@ -41,7 +31,6 @@ Sync () {
   done
   Spam
 }
-
 Spam () {
 	echo ""
 	echo ""
@@ -63,18 +52,50 @@ Spam () {
 	EchoRed "| (___) | (____/\  | |     | )___) ) (____/\__) (___/   (_/\  | |  | (____/\ (__/  )        "
 	EchoRed "(_______|_______/  )_(     |/ \___/(_______|_______(_______/  )_(  (_______(______(_)      "
 }
-
-Build () {
-	scripts/build.sh "$1" "$2"
+Usage () {
+  echo " "
+  EchoRed "USAGE: Valid arguments are:"
+  EchoGreen "-s(ync)"
+  EchoGreen "-b(uild) <all/treble/nontreble>"
+  EchoGreen "-v(ersion) <version>"
+  EchoGreen "-d(epth) <sync depth for kernel repo>"
+  EchoRed "Sync, build, or both must be specified"
+  echo " "
+  unset KRIEG_SCRIPT
+  exit 1
 }
 
-if [ "$1" = "sync" ]; then
+export KRIEG_SCRIPT="true"
+KRIEG_ROOT=`pwd`
+TEXTRESET=$(tput sgr0)
+TEXTGREEN=$(tput setaf 2)
+TEXTRED=$(tput setaf 1)
+buildshit=false; syncshit=false
+
+while true; do
+  case "$1" in
+    -b) buildshit=true; shift; buildtype="$1"; shift;;
+    -s) syncshit=true; shift;;
+    -v) shift; version="$1"; shift;;
+    -d) shift; depth=" --depth=$1"; shift;;
+    "") shift; break;;
+    *) EchoRed "Unsupported flag $1!"; Usage;;
+  esac
+done
+
+[ -z "$version" ] && { EchoRed "NO Version number specified. Naming zips as TEST}"; version="TEST"; }
+[ -z "$depth" ] && { EchoRed "Depth not specified. Pulling everything"; }
+
+if $syncshit && $buildshit; then
+  Sync
+	scripts/build.sh "$buildtype" "$version"
+elif $syncshit && ! $buildshit; then
 	Sync
-elif [ "$1" = "build" ]; then
-	Build "$2" "$3"
+elif $buildshit; then
+	scripts/build.sh "$buildtype" "$version"
 else
-	Sync
-	Build "$1" "$2"
+  EchoRed "No action specified!"
+  Usage
 fi
 
 unset KRIEG_SCRIPT
